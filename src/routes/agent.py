c8328223aff9b533faeb3f7c84d8d552bd9c19a2
neo2100 <mohammad_sh2100@yahoo.com>
2025-01-18 16:54:53 +0100

    Add rest client test APIs and fixing the APIs

--- src/routes/agent.py
+++ src/routes/agent.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from typing import List
 from src.agent_proxy.types import Agent, Message
 from src.agent_proxy import agent_proxy
+from fastapi import HTTPException, status, Body
 
 router = APIRouter()
 
@@ -9,7 +10,11 @@ router = APIRouter()
 def read_agents():
     return agent_proxy.get_available_agents()
 
-
 @router.post("/agent", response_model=Message)
-def message_agent():
-    return agent_proxy.message_agent(agent_id="1_dictionary", user_message = {"content": "test"} )
+def message_agent(user_message: Message = Body(...)):
+    if not user_message or not user_message.content:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid message format"
+        )
+    return agent_proxy.message_agent(agent_id="1_dictionary", user_message=user_message)
