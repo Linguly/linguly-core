91331affbb773abf1b9e1832a62d50a9e4ec5d71
Hadi <65487316+neo2100@users.noreply.github.com>
2025-05-31 18:32:58 +0200

    Revert "1 user authentication"

--- src/model_proxy/types.py
+++ src/model_proxy/types.py
@@ -1,12 +1,11 @@
+
 from pydantic import BaseModel
 from typing import List
-
 
 class ModelConnector(BaseModel):
     """
     Agent metadata to return based on available agents
     """
-
     id: str
     type: str
     display_name: str
@@ -14,8 +13,7 @@ class ModelConnector(BaseModel):
     category: List[str]
     interaction_type: List[str]
     supported_languages: List[str]
-
-
+    
 class Message(BaseModel):
     role: str
     content: str
