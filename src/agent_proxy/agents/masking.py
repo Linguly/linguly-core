384a526c8810d8723ed7c0386548b1cf3f5bb193
neo2100 <mohammad_sh2100@yahoo.com>
2025-06-29 18:47:13 +0200

    masking + validation

--- src/agent_proxy/agents/masking.py
+++ src/agent_proxy/agents/masking.py
@@ -1,9 +1,10 @@
 from src.agent_proxy.types import Agent, Message
 from src.model_proxy import model_proxy
+from src.db_proxy.db_proxy import get_db
 from src.shared_context.types import Goal
 from src.shared_context.learning_phrases import LearningPhrases
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, ClassVar
 import json
 
 learning_phrases = LearningPhrases()
@@ -30,6 +31,7 @@ class Masking(Agent):
     shared_context_field: str = "learning_phrases"
     compatible_interfaces: List[str] = ["web", "telegram"]
     config: Configuration
+    db: ClassVar = get_db("agent_session")
 
     def __init__(self, **data):
         # Call the parent class constructor with the modified data
@@ -50,10 +52,37 @@ class Masking(Agent):
     def get_next_learning_phrase(self, user_id: str, goal_id: str):
         return learning_phrases.get_next_learning_phrases(user_id, goal_id, top=1)
 
+    def store_next_phrase(self, user_id: str, goal_id: str, next_phrase: str):
+        existing_session = self.db.find(
+            "masking_agent",
+            {"user_id": user_id, "goal_id": goal_id},
+            limit=1,
+        )
+        if not existing_session:
+            self.db.insert(
+                "masking_agent",
+                {
+                    "user_id": user_id,
+                    "goal_id": goal_id,
+                    "last_phrase": next_phrase.lower(),
+                },
+            )
+        else:
+            self.db.update(
+                "masking_agent",
+                {
+                    "user_id": user_id,
+                    "goal_id": goal_id,
+                },
+                {"$set": {"last_phrase": next_phrase.lower()}},
+            )
+
     def generate_next_masking_text(self, user_id: str, user_goal: Goal) -> str:
         next_phrase = self.get_next_learning_phrase(user_id, user_goal.id)[0]
-        learning_language = user_goal.language
-        masking_user_message = Message(
+        # Store next_phrase to check it later
+        self.store_next_phrase(user_id, user_goal.id, next_phrase)
+        # Generate text to be masked
+        next_phrase_user_message = Message(
             content=self.user_prompt(
                 user_goal,
                 next_phrase,
@@ -61,9 +90,10 @@ class Masking(Agent):
             role="user",
         )
         model_response_content = self.model_connector.reply(
-            messages=[masking_user_message]
+            messages=[next_phrase_user_message]
         ).content
-        # mask the next_phrase
+
+        # Mask the next_phrase
         splitted_phrase = next_phrase.split()
         for split in splitted_phrase:
             model_response_content = model_response_content.replace(split, "`______`")
@@ -74,6 +104,26 @@ class Masking(Agent):
             print("WARNING: Couldn't find the phrase in the user context")
             # TODO: proper action required here (e.g. regenerate the text)
             return model_response_content
+
+    def validate(self, user_id: str, goal_id: str, user_answer: str) -> str:
+        session = self.db.find(
+            "masking_agent",
+            {"user_id": user_id, "goal_id": goal_id},
+            limit=1,
+        )
+        last_phrase = (
+            session[0]["last_phrase"]
+            if session and session[0] and "last_phrase" in session[0]
+            else None
+        )
+        
+        if not last_phrase:
+            raise ValueError("Previous session or last_phrase is not stored!")
+        is_correct = last_phrase == user_answer.lower()
+        learning_phrases.update_learning_status(
+            user_id, goal_id, last_phrase, is_correct
+        )
+        return last_phrase
 
     @property
     def model_connector(self):
@@ -89,13 +139,11 @@ class Masking(Agent):
     def reply(
         self, user_id: str, user_goal: Goal, messages: List[Message]
     ) -> List[Message]:
-        user_message = messages[0]
-        # TODO: validate the answer!
-
+        # Validate user answer
+        last_phrase = self.validate(user_id, user_goal.id, messages[0].content)
+        # Prepare next text
         masking_text = self.generate_next_masking_text(user_id, user_goal)
-        # TODO: return previous phrase before sending next text
-        return [Message(content=masking_text, role="assistant")]
-
-    def validate(self, user_id: str, user_message: Message, user_goal: Goal):
-        # TODO: validate if the user message match the phrase and record the result in learning_phrases + return the phrase
-        pass
+        return [
+            Message(content=last_phrase, role="assistant"),
+            Message(content=masking_text, role="assistant"),
+        ]
